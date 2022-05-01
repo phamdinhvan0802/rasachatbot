5bfada231267211458b81d42aa3450bbdbb29b14
Phạm Đình VĂn <phamdinhvan0802@gmail.com>
2022-05-01 15:13:39 +0700

    rasa commit

---
+++ actions/actions.py
@@ -0,0 +1,52 @@
+﻿# This files contains your custom actions which can be used to run
+# custom Python code.
+#
+# See this guide on how to implement these action:
+# https://rasa.com/docs/rasa/custom-actions
+
+
+# This is a simple example for a custom action which utters "Hello World!"
+
+# from typing import Any, Text, Dict, List
+#
+# from rasa_sdk import Action, Tracker
+# from rasa_sdk.executor import CollectingDispatcher
+import random
+
+DATABASE = ["bún đậu mắm tôm",
+            "bún đậu nước mắm",
+            "bún cá",
+            "bún hải sản",
+            "cơm văn phòng",
+            "cơm sườn",
+            "xôi",
+            "bún ốc",
+            "mì vằn thắn",
+            "hủ tiếu",
+            "bún chả",
+            "bún ngan",
+            "ngan xào tỏi",
+            "bún bò huế",
+            "mì tôm hải sản",
+            "bánh mì trứng xúc xích rắc thêm ít ngải cứu",
+            "bánh mì trứng",
+            "bánh mì xúc xích",
+            "bánh mì pate"]
+class ActionRecommend(Action):
+
+    def name(self) -> Text:
+        return "action_recommend"
+
+    def run(self, dispatcher: CollectingDispatcher,
+            tracker: Tracker,
+            domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
+
+        food = []
+        for i in range(2):
+            food_number = random.randrange(len(DATABASE))
+            food.append(DATABASE[food_number])
+
+        dispatcher.utter_message(
+            text="Em nghĩ hôm nay anh chị có thể thử món '{}' hoặc bên cạnh đó cũng có thể là món '{}' ạ".format(food[0], food[1]))
+
+        return []
